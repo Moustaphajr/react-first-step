210e66b3c8205a41542fbe31b45ab95f8c75c597
Moustapha <mouhamedjr2012@gmail.com>
2023-04-08 17:41:50 +0000

    first-commit

---
+++ src/Element.js
@@ -0,0 +1,12 @@
+
+// eslint-disable-next-line no-global-assign
+export default Element=[
+    {
+        titre:"hello",
+        body:"world"
+    },
+     {
+        titre:"Happy",
+        body:"coding"
+    }
+]
